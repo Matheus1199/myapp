8f81a9c5fa8990c1490cbc7e6f0002a7b5f4400e
Matheus1199 <89140928+Matheus1199@users.noreply.github.com>
2022-03-08 17:22:06 -0300

    primeiros códigos

---
+++ Sintaxe/variaveis.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Programming
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            //Variveis
+            int primeironumero = 50;
+            string primeirotexto = "Olá mundo";
+            float primeironumerofloat = 50.32f;
+        }
+    }
+}
